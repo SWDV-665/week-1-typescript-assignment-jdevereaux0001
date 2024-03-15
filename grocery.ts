f260c9e0dc9844519d32ff0dc93d956470776cbe
jdevereaux0001 <143358192+jdevereaux0001@users.noreply.github.com>
2024-03-14 19:38:47 -0500

    Update grocery.ts

--- grocery.ts
+++ grocery.ts
@@ -1,1 +1,10 @@
-placeholder
+class Grocery {
+  name: string;
+  quantity: integer;
+  dept: string;
+
+  var values: Array = ['Milk', 20, 'Bread', 40, 'Eggs', 30];
+}
+const groceryStore = new Grocery();
+groceryStore.name = "Jasons Grocery Store";
+groceryStore.dept = "Produce";
